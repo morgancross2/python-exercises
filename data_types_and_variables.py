c81a167581b8bb10a0cc91e525c8977192ca769c
Morgan Cross <morgan.e.cross2@gmail.com>
2022-07-12 16:17:52 -0500

    Completed bonus

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -12,15 +12,25 @@ total_payment = 400*6 + 380*4 + 350*10
 
 # 3. A student can be enrolled to a class only if the class is not full and the class schedule does not 
 # conflict with her current schedule.
-# enrolled if class_not_full == true and no_schedule_conflict == true
+class_not_full = True
+no_schedule_conflict = True
+
+enrolled = (class_not_full and no_schedule_conflict)
+print(f'Is the student enrolled? {enrolled}')
 
 # 4. A product offer can be applied only if people buys more than 2 items, and the offer has not expired. 
 # Premium members do not need to buy a specific amount of products
-# offer if premium member or (bought > 2 and offer_not_expired == true)
+premium_member = True
+bought = 3
+offer_not_expired = True
+
+
+get_offer = premium_member or (bought > 2 and offer_not_expired)
+print(f'Can I apply the offer? {get_offer}')
 
 # 5. Create a variable that holds a boolean value for each of the following conditions:
 
-username = 'codeup'
+username = 'codeup  '
 password = 'notastrongpassword'
 
 # the password must be at least 5 characters
@@ -33,3 +43,16 @@ not_same = password != username
 print('Password 5 characters or more? ' + str(long_enough))
 print('Username 20 characters or less? ' + str(not_too_long))
 print('Password different than username? ' + str(not_same))
+
+
+# 6. bonus neither the username or password can start or end with whitespace
+if password == password.strip():
+    print('Password is good!')
+else:
+    print('Password cannot have whitespace at start or end.')
+
+
+if username == username.strip():
+    print('Username is good!')
+else:
+    print('Username cannot have whitespace at start or end.')
